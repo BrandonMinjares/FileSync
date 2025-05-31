b94083ccaed9826496b1eaf6031e12b929f04e33
Brandon Minjares <bran@iMac.attwifi.manager>
2025-05-30 23:40:55 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module synthesize
+
+go 1.24.3
+
+require (
+	github.com/fsnotify/fsnotify v1.9.0
+	golang.org/x/sys v0.13.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	// Print current directory
+	cwd, _ := os.Getwd()
+	fmt.Println("Current working dir:", cwd)
+
+	// Create watcher
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	// Start goroutine to listen for events
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				fmt.Println("EVENT:", event)
+				if event.Op&fsnotify.Create == fsnotify.Create {
+					fmt.Println("File created:", event.Name)
+				}
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					fmt.Println("File modified:", event.Name)
+				}
+				if event.Op&fsnotify.Remove == fsnotify.Remove {
+					fmt.Println("File deleted:", event.Name)
+				}
+				if event.Op&fsnotify.Rename == fsnotify.Rename {
+					fmt.Println("File renamed:", event.Name)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				fmt.Println("ERROR:", err)
+			}
+		}
+	}()
+
+	// Add the folder to watcher
+	fmt.Println("Watching folder: ./tmp")
+	err = watcher.Add("./tmp")
+	if err != nil {
+		log.Fatal("Failed to add watcher:", err)
+	}
+	fmt.Println("Successfully watching ./tmp")
+
+	// Block main from exiting
+	<-make(chan struct{})
+}
