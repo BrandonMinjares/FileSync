256f8a68b446cd2db69689cbe9237ca05c3b631a
Brandon Minjares <bran@iMac.attwifi.manager>
2025-06-05 02:16:36 -0700

    Added watcher.Add code to begin watching folder

--- main.go
+++ main.go
@@ -3,52 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strings"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Print(`
-	Would you like to:
-	1. Create a new bucket
-	2. Add a folder to a bucket
-	3. Connect to a new user
-	> `)
-	input, _ := reader.ReadString('\n')
-	input = strings.TrimSpace(input)
-
-	switch input {
-	case "1":
-		fmt.Print(`
-		What would you like the bucket name to be?`)
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
-		CreateBucket("my.db", input)
-	case "2":
-		fmt.Print(`
-		What folder would you like to add?`)
-		folder, _ := reader.ReadString('\n')
-		folder = strings.TrimSpace(folder)
-
-		fmt.Print(`
-		To which bucket?`)
-		bucket, _ := reader.ReadString('\n')
-		bucket = strings.TrimSpace(bucket)
-		AddFolderToBucket("my.db", folder, bucket)
-	default:
-		fmt.Println("Invalid selection. Exiting.")
-		return
-	}
-}
-
-/***
-
-	cwd, _ := os.Getwd()
-	fmt.Println("Current working dir:", cwd)
-
-	// Create watcher
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -73,11 +35,9 @@ func main() {
 						fmt.Println("Error:", err)
 						return
 					}
-
 					fmt.Println("File Name:", info.Name())
 					fmt.Println("Size (bytes):", info.Size())
 					fmt.Println("Last Modified:", info.ModTime())
-
 				}
 				if event.Op&fsnotify.Remove == fsnotify.Remove {
 					fmt.Println("File deleted:", event.Name)
@@ -94,6 +54,61 @@ func main() {
 		}
 	}()
 
-	// Block main from exiting
-	<-make(chan struct{})
-**/
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print(`
+				Would you like to:
+				1. Create a new bucket
+				2. Add a folder to a bucket
+				3. Connect to a new user
+				Type "exit" to quit
+				> `)
+
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		switch input {
+		case "1":
+			fmt.Print("What would you like the bucket name to be? ")
+			input, _ := reader.ReadString('\n')
+			input = strings.TrimSpace(input)
+			if err := CreateBucket("my.db", input); err != nil {
+				log.Println("Error creating bucket:", err)
+			} else {
+				fmt.Println("Bucket created.")
+			}
+
+		case "2":
+			fmt.Print("What folder would you like to add? ")
+			folder, _ := reader.ReadString('\n')
+			folder = strings.TrimSpace(folder)
+
+			fmt.Print("To which bucket? ")
+			bucket, _ := reader.ReadString('\n')
+			bucket = strings.TrimSpace(bucket)
+
+			if err := AddFolderToBucket("my.db", folder, bucket); err != nil {
+				log.Println("Error adding folder to bucket:", err)
+			} else {
+				fmt.Println("Folder added to bucket.")
+			}
+			fmt.Printf("Watching folder: %s", folder)
+			err = watcher.Add(folder)
+			if err != nil {
+				log.Fatal("Failed to add watcher:", err)
+			}
+			fmt.Printf("Successfully watching %s", folder)
+
+		case "3":
+			fmt.Println("Feature not implemented yet.")
+
+		case "exit":
+			fmt.Println("Exiting.")
+			return
+
+		default:
+			fmt.Println("Invalid selection. Try again.")
+		}
+	}
+}
