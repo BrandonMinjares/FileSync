0d8b7538894630b94512286f6add2b5cce5c4276
Brandon Minjares <bran@iMac.attlocal.net>
2026-03-16 00:09:59 -0700

    Readded RequestConnection function for full trusted handshake

--- grpc_client.go
+++ grpc_client.go
@@ -134,6 +134,26 @@ func (s *server) notifyPeerTrusted(peerDeviceID string) error {
 	return err
 }
 
+func (s *server) RequestConnection(ctx context.Context, req *pb.ConnectionRequest) (*pb.ConnectionResponse, error) {
+	fmt.Printf("Incoming connection request from %s (%s)\n", req.RequesterName, req.RequesterId)
+
+	if pi, exists := s.user.Peers[req.RequesterId]; exists && pi.State == SEEN {
+		if err := s.PromotePeerToPendingAcceptance(req.RequesterId); err != nil {
+			log.Printf("failed to promote to trusted: %v", err)
+			return &pb.ConnectionResponse{Accepted: false, Message: "Internal error"}, nil
+		}
+		return &pb.ConnectionResponse{
+			Accepted: true,
+			Message:  "Connection pending!",
+		}, nil
+	}
+
+	return &pb.ConnectionResponse{
+		Accepted: false,
+		Message:  "Connection denied!",
+	}, nil
+}
+
 func (s *server) FileUpdateRequest(
 	filePath string,
 	id string,
